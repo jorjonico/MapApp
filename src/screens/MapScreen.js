9ce4142c6cdf36be6bb329a2b0230fbc96fed527
Jorge Matinez <jorjonicodcv@gmail.com>
2022-12-23 14:37:16 -0300

    con maps

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,17 +1,38 @@
+import MapView, { Marker } from "react-native-maps";
+import React, {useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import MapView from "react-native-maps";
-import React from "react";
-
-const MapScreen = () => {
-  const initialRegion = {
-    latittude: 37.4219023,
+const initialRegion = {
+    latitude: 37.4219023,
     longitude: -122.0839984,
-    latittudeDelta: 0.0922,
+    latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  
-  return <MapView initialRegion={initialRegion} style={styles.container} />;
+const MapScreen = () => {
+  const [selectedLocation, setSelectedLocation] = useState("")
+  const handleSelectedLocation = (event) => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude,
+    });
+  };
+  return (
+    <MapView
+      initialRegion={initialRegion}
+      style={styles.container}
+      onPress={handleSelectedLocation}
+      >
+      {selectedLocation && (
+        <Marker 
+        title="Ubicación seleccionada" 
+        coordinate={{
+          latitude: selectedLocation.lat, 
+          longitude: selectedLocation.lng
+        }}
+        />
+      )}
+      </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
